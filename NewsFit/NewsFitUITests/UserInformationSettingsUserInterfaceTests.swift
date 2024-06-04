200c1b9ae87bbc2ab80237b6182c807af95bfe8b
iceHood <71179395+iceHood@users.noreply.github.com>
2024-06-04 11:41:57 +0900

    feature: Test withdrawal UI

--- NewsFit/NewsFitUITests/UserInformationSettingsUserInterfaceTests.swift
+++ NewsFit/NewsFitUITests/UserInformationSettingsUserInterfaceTests.swift
@@ -61,3 +61,73 @@ final class when_user_click_on_logout_cell: XCTestCase {
 
 //MARK: - About Withdrawal
 
+/// 회원탈퇴 기능 테스트
+/// 확인 누르면 -> 로그인 화면으로 나가짐
+/// 취소 누르면 설정화면으로 이동
+final class when_user_click_on_withdrawal_cell: XCTestCase {
+    private var app: XCUIApplication!
+    private var settingPageObject: UserInformationSettingsPageObject!
+    private let menuCount = 6
+    
+    override func setUp() {
+        app = .init()
+        continueAfterFailure = false
+        
+        settingPageObject = .init(app: app)
+        let loginPageObject = LoginPageObject(app: app)
+        let newsPageObject = NewsFitNewsPageObject(app: app)
+        
+        app.launchEnvironment = ["ENV" : "DEV"]
+        
+        app.launch()
+        
+        let kakaoLoginButton = loginPageObject.kakaoLoginButton
+        
+        _ = kakaoLoginButton.waitForExistence(timeout: 1)
+        kakaoLoginButton.tap()
+        
+        _ = loginPageObject.mainLogoImage.waitForExistence(timeout: 0.5)
+        
+        let myInfoNavButton = newsPageObject.myInfoNavButton
+        myInfoNavButton.tap()
+    }
+    
+    func test_should_display_login_page_with_confirm() {
+        let withdrawalButton = settingPageObject.myInfoCell(at: menuCount-1)
+        _ = withdrawalButton.waitForExistence(timeout: 1)
+        
+        withdrawalButton.tap()
+        
+        let confirmButton = settingPageObject.confirmButton
+        _ = confirmButton.waitForExistence(timeout: 1)
+        
+        confirmButton.tap()
+        
+        let realConfirmButton = settingPageObject.confirmButton
+        _ = realConfirmButton.waitForExistence(timeout: 1)
+        
+        realConfirmButton.tap()
+        
+        let kakaoLoginButton = LoginPageObject(app: app).kakaoLoginButton
+        XCTAssertTrue(kakaoLoginButton.waitForExistence(timeout: 3))
+    }
+    
+    func test_should_navigate_to_my_information_page_with_cancel() {
+        let withdrawalButton = settingPageObject.myInfoCell(at: menuCount-1)
+        _ = withdrawalButton.waitForExistence(timeout: 1)
+        
+        withdrawalButton.tap()
+        
+        let confirmButton = settingPageObject.confirmButton
+        _ = confirmButton.waitForExistence(timeout: 1)
+        
+        confirmButton.tap()
+        
+        let cancelButton = settingPageObject.cancelButton
+        _ = cancelButton.waitForExistence(timeout: 1)
+        
+        cancelButton.tap()
+        
+        XCTAssertTrue(withdrawalButton.waitForExistence(timeout: 1))
+    }
+}
