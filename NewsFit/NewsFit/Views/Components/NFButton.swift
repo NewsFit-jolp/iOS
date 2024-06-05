9f258f8538bcf49b32a798a5f0fbfdef9cee3383
iceHood <71179395+iceHood@users.noreply.github.com>
2024-06-05 16:53:41 +0900

    chore: Create file - NFButton

---
+++ NewsFit/NewsFit/Views/Components/NFButton.swift
@@ -0,0 +1,22 @@
+//
+//  NFButton.swift
+//  NewsFit
+//
+//  Created by User on 6/4/24.
+//
+
+import UIKit
+
+final class NFButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
