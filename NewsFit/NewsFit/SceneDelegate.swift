dfaaebc0709cce1ea111ce28837dda26a0a1f8d4
iceHood <71179395+iceHood@users.noreply.github.com>
2024-07-02 16:26:50 +0900

    feature: Configure Login buttons

--- NewsFit/NewsFit/SceneDelegate.swift
+++ NewsFit/NewsFit/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: scene)
-        let vc = UIViewController()
+        let vc = LoginViewController()
         vc.view.backgroundColor = .blue
         window?.rootViewController = vc
         
--- NewsFit/NewsFit/ViewControllers/LoginViewController.swift
+++ NewsFit/NewsFit/ViewControllers/LoginViewController.swift
@@ -6,26 +6,65 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 //MARK: - LoginViewController
 final class LoginViewController: UIViewController {
     //MARK: - Properties
-    private let naverLoginButton = NFButton(with: .naverLogin(isSmall: false))
-    private let kakaoLoginButton = NFButton(with: .kakaoLogin(isSmall: false))
-    private let appleLoginButton = NFButton(with: .appleLogin(isSmall: false))
+    private let naverLoginButton: NFButton = {
+        let btn = NFButton(with: .naverLogin(isSmall: false))
+        
+        // add Views to button
+        let img = UIImageView(image: .init(named: "naver_logo_white"))
+        img.frame = .init(x: 0, y: 0, width: 16, height: 16)
+        img.contentMode = .scaleAspectFill
+        
+        let lb = NFLabel()
+        lb.setText("네이버 로그인", with: .button)
+        
+        btn.setTitle(views: [img, lb], spacing: 10)
+        return btn
+    }()
+    private let kakaoLoginButton: NFButton =  {
+        let btn = NFButton(with: .kakaoLogin(isSmall: false))
+        let img = UIImageView(image: .init(named: "kakao_logo"))
+        img.frame = .init(x: 0, y: 0, width: 18, height: 18)
+        img.contentMode = .scaleAspectFill
+        
+        let lb = NFLabel()
+        lb.setText("카카오 로그인", with: .button)
+        
+        btn.setTitle(views: [img, lb], spacing: 10)
+        return btn
+    }()
+    private let appleLoginButton: ASAuthorizationAppleIDButton = {
+        let btn = ASAuthorizationAppleIDButton(authorizationButtonType: .continue, authorizationButtonStyle: .black)
+        btn.cornerRadius = 8
+        
+        return btn
+    }()
     
-    private let naverRegisterButton = NFButton(with: .naverLogin(isSmall: true))
-    private let kakaoRegisterButton = NFButton(with: .kakaoLogin(isSmall: true))
-    private let appleRegisterButton = NFButton(with: .appleLogin(isSmall: true))
+    private let naverRegisterButton: NFButton = {
+        let btn = NFButton(with: .naverLogin(isSmall: true))
+        return btn
+    }()
+    private let kakaoRegisterButton: NFButton = {
+        let btn = NFButton(with: .kakaoLogin(isSmall: true))
+        return btn
+    }()
+    private let appleRegisterButton: NFButton = {
+        let btn = NFButton(with: .appleLogin(isSmall: true))
+        return btn
+    }()
     
     private let logo: UIImageView = {
-        let uv = UIImageView(image: UIImage(named: "NFLogo")!)
+        let uv = UIImageView(image: UIImage(named: "NFLogo"))
         uv.contentMode = .scaleAspectFit
         uv.frame = .init(x: 0, y: 0, width: 105, height: 105)
         return uv
     }()
     private let appImage: UIImageView = {
-        let uv = UIImageView(image: UIImage(named: "AppIcon")!)
+        let uv = UIImageView(image: UIImage(named: "AppIcon"))
         uv.contentMode = .scaleAspectFill
         uv.frame = .init(x: 0, y: 0, width: 190, height: 68)
         return uv
@@ -46,7 +85,7 @@ final class LoginViewController: UIViewController {
         //login buttons
         naverLoginButton.addTarget(action: #selector(naverLoginTapped))
         kakaoLoginButton.addTarget(action: #selector(naverLoginTapped))
-        appleLoginButton.addTarget(action: #selector(naverLoginTapped))
+//        appleLoginButton.addTarget(action: #selector(naverLoginTapped))
         
         // register buttons
         naverRegisterButton.addTarget(action: #selector(naverRegisterTapped))
@@ -65,6 +104,7 @@ final class LoginViewController: UIViewController {
         logoStack.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalToSuperview().offset(137)
+            make.width.equalTo(190)
         }
         // 2. 로그인 버튼들을 설정한다.
         let loginStack = UIStackView(arrangedSubviews: [naverLoginButton, kakaoLoginButton, appleLoginButton])
@@ -76,6 +116,7 @@ final class LoginViewController: UIViewController {
         loginStack.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalTo(logoStack.snp.bottom).offset(115)
+            make.width.equalTo(331)
         }
         
         
@@ -85,13 +126,14 @@ final class LoginViewController: UIViewController {
     //MARK: - Selectors
     @objc private func naverLoginTapped() {
         // login Logics Here
-        
+        print(#function)
         // Turn to next page
         turnToNext()
     }
     
     @objc private func naverRegisterTapped() {
         // login Logics Here
+        print(#function)
         
         // Turn to next page
         turnToNext()
