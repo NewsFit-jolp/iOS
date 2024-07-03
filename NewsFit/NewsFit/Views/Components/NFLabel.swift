f058192acb530d4871d954fa54873140326df318
iceHood <71179395+iceHood@users.noreply.github.com>
2024-07-03 13:09:04 +0900

    feature: Add color selection to NFLabel

--- NewsFit/NewsFit/Views/Components/NFLabel.swift
+++ NewsFit/NewsFit/Views/Components/NFLabel.swift
@@ -32,9 +32,27 @@ enum NFLabelFont {
     }
 }
 
+enum NFLabelColor {
+    case dark
+    case gray
+    case white
+    
+    var color: UIColor {
+        switch self {
+        case .dark:
+                .textDark
+        case .gray:
+                .textGray
+        case .white:
+                .textWhite
+        }
+    }
+}
+
 final class NFLabel: UIView {
     private var label = UILabel()
     private var font: NFLabelFont = .body
+    private var textColor: NFLabelColor = .dark
     private var isAddedSubView: Bool = false
     private var isUnderLined: Bool = false
     
@@ -51,7 +69,7 @@ final class NFLabel: UIView {
         label.font = font.font
         
         if !isAddedSubView {
-            label.textColor = .textDark
+            label.textColor = textColor.color
             label.textAlignment = .left
             // add subview
             addSubview(label)
@@ -67,19 +85,26 @@ final class NFLabel: UIView {
         setup(font: font)
     }
     
+    func setColor(with color: NFLabelColor) {
+        label.textColor = color.color
+    }
+    
     func setUnderLine(to set: Bool) {
         isUnderLined = set
         let text = label.text ?? label.attributedText?.string ?? ""
         if isUnderLined { // 기본글자 -> 속성 글자
-            let attributeText = NSAttributedString(string: text, attributes: [.font: self.font.font,
-                .foregroundColor: UIColor.textDark,
-                .underlineStyle: NSUnderlineStyle.single.rawValue]
-            )
+            let attributeText = NSAttributedString(
+                string: text,
+                attributes: [
+                    .font: self.font.font,
+                    .foregroundColor: textColor.color,
+                    .underlineStyle: NSUnderlineStyle.single.rawValue
+                ])
             label.attributedText = attributeText
         } else { // 속성 글자 -> 기본글자
             label.text = text
             label.font = self.font.font
-            label.textColor = .textDark
+            label.textColor = textColor.color
         }
     }
     
