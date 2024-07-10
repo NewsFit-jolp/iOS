65e9b580a9d4e5eddcbd6052ff892f5fe507805f
iceHood <71179395+iceHood@users.noreply.github.com>
2024-07-10 16:21:33 +0900

    fix: Boarderble view bug fixed

--- NewsFit/NewsFit/Views/Components/PropertyWrapper/Borderble.swift
+++ NewsFit/NewsFit/Views/Components/PropertyWrapper/Borderble.swift
@@ -15,7 +15,7 @@ class Borderble<T: UIView>: UIView {
         lb.setColor(with: .grayDark)
         lb.isHidden = true
         lb.backgroundColor = .backgroundWhite
-        lb.setMargin(about: 8)
+        lb.setMargin(width: 10)
         return lb
     }()
     
@@ -36,22 +36,23 @@ class Borderble<T: UIView>: UIView {
     }
     
     private func setup() {
+        
         addSubview(wrappedValue)
-        wrappedValue.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+        addSubview(title)
+        title.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(10)
+            make.top.equalToSuperview()
         }
+        
         
         wrappedValue.layer.borderColor = UIColor.borderGray.cgColor
         wrappedValue.layer.borderWidth = 2
         wrappedValue.layer.cornerRadius = 10
-        
-        
-        addSubview(title)
-        title.snp.makeConstraints { make in
-            make.left.equalTo(wrappedValue.snp.left).offset(10)
-            make.top.equalTo(wrappedValue.snp.top).offset(-10)
-            
+        wrappedValue.snp.makeConstraints { make in
+            make.top.equalTo(title.snp.bottom).offset(-10)
+            make.left.right.bottom.equalToSuperview()
         }
+        
     }
     
     func setTitle(with text: String) {
