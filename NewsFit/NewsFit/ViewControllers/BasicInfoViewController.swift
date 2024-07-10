5987f10186f81ab7aae0355db2f040135ae7b8ee
iceHood <71179395+iceHood@users.noreply.github.com>
2024-07-10 16:24:08 +0900

    feature: Complete BasicInfoView

--- NewsFit/NewsFit/ViewControllers/BasicInfoViewController.swift
+++ NewsFit/NewsFit/ViewControllers/BasicInfoViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 final class BasicInfoViewController: UIViewController {
     //MARK: - Properties
     private let infoView = BasicInfoView()
+    private let nextButton: NFButton = {
+        let btn = NFButton(with: .primary(isHalf: false))
+        let lb = NFLabel()
+        lb.setText("계속", with: .button)
+        lb.setColor(with: .white)
+        btn.setTitle(views: [lb], spacing: 0)
+        
+        return btn
+    }()
     
     //MARK: - LifeCycle
     override func viewDidLoad() {
@@ -19,15 +28,20 @@ final class BasicInfoViewController: UIViewController {
     //MARK: - Helper
     private func setup() {
         self.view.backgroundColor = .backgroundWhite
-        setConstraint()
+        configureView()
     }
     
-    private func setConstraint() {
+    private func configureView() {
         self.view.addSubview(infoView)
         infoView.snp.makeConstraints { make in
-            make.horizontalEdges.equalToSuperview().inset(31)
-            make.top.equalToSuperview().offset(36)
-            
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(31)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(36)
+        }
+        
+        self.view.addSubview(nextButton)
+        nextButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-38)
         }
     }
 }
