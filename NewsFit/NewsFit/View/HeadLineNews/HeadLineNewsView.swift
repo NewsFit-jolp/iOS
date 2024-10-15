3ea936b2b302920f416a2ef5c49bebe17657ccc8
JungHyun Lim <hanyo3477@gmail.com>
2024-10-15 17:29:57 +0900

    feat: HeadLineView 생성

---
+++ NewsFit/NewsFit/View/HeadLineNews/HeadLineNewsView.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct HeadLineNewsView: View {
+  var body: some View {
+    VStack {
+      Spacer()
+      VStack(alignment: .leading) {
+        Text("\"최악의 기후재앙\"...브라질 남부 폭우에 사망.실종 220명 넘어서")
+          .font(.headline)
+          .padding(.bottom)
+        Text("부상자 361명, 15만5천명 대피")
+      }
+      Spacer()
+      HStack {
+        Text("한겨레")
+        Spacer()
+        Text("2024.05.28")
+      }
+    }.padding()
+  }
+}
+
+#Preview {
+  HeadLineNewsView()
+}
