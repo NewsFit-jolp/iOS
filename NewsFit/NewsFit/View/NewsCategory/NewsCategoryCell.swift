d31e5912f433ae7f490ce5606de69d5439315f86
JungHyun Lim <hanyo3477@gmail.com>
2024-10-17 15:16:18 +0900

    feat: Cell UI 작업 완료

--- NewsFit/NewsFit/View/NewsCategory/NewsCategoryCell.swift
+++ NewsFit/NewsFit/View/NewsCategory/NewsCategoryCell.swift
@@ -2,10 +2,20 @@ import SwiftUI
 
 struct NewsCategoryCell: View {
   var body: some View {
-    Text("NewsCategoryCell")
+    Text("전체")
+      .padding(.horizontal, 20)
+      .padding(.vertical, 10)
+      .background(Color.nfPurple)
+      .foregroundStyle(.white)
+      .clipShape (
+        RoundedRectangle(cornerRadius: 16)
+      )
+      .overlay {
+        RoundedRectangle(cornerRadius: 16)
+          .stroke(Color.nfBorderDefault)
+      }
   }
 }
-
 
 #Preview {
   NewsCategoryCell()
