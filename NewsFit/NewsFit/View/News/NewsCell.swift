15cc81a2adc2cd23262ad44d74177ef8f156a566
JungHyun Lim <hanyo3477@gmail.com>
2024-10-21 16:45:39 +0900

    feat: ViewModel과 Cell 연결

--- NewsFit/NewsFit/View/News/NewsCell.swift
+++ NewsFit/NewsFit/View/News/NewsCell.swift
@@ -1,33 +1,55 @@
 import SwiftUI
 
 struct NewsCell: View {
+  //MARK: - Property
+  @State var viewModel: NewsPresentable & PressImagePresentable
+  
+  //MARK: - View
   var body: some View {
     HStack {
-      Image(.successMark)
+      imageView()
+      VStack(alignment: .leading) {
+        Text(viewModel.title)
+          .font(.headline)
+          .lineLimit(2)
+        descriptionView(viewModel)
+      }
+    }
+  }
+  
+  //MARK: - Helpers
+  @ViewBuilder
+  private func imageView() -> some View {
+    Image(.successMark)
+      .resizable()
+      .scaledToFit()
+      .frame(maxWidth: 70, maxHeight: 70)
+      .background(Color.black)
+      .clipShape(RoundedRectangle(cornerRadius: 16))
+      .padding(.trailing, 20)
+  }
+  @ViewBuilder
+  private func descriptionView(_ viewModel: NewsPresentable & PressImagePresentable) -> some View {
+    HStack(spacing: 10) {
+      Image(.kakaoLogo)
         .resizable()
         .scaledToFit()
-        .frame(maxWidth: 70, maxHeight: 70)
-        .background(Color.black)
-        .clipShape(RoundedRectangle(cornerRadius: 16))
-        .padding(.trailing, 20)
-      VStack(alignment: .leading) {
-        Text("고양이가 그렇게 귀엽다며... 사료를 1년치 주문... 충격")
-          .font(.headline)
-        HStack(spacing: 10) {
-          Image(.kakaoLogo)
-            .resizable()
-            .scaledToFit()
-            .frame(minWidth: 20, maxWidth: 20)
-          Text("카카오또-끄")
-            .bold()
-          Text("·")
-          Text("3시간전")
-        }
-      }
+        .frame(minWidth: 20, maxWidth: 20)
+      Text(viewModel.press)
+        .bold()
+      Text("·")
+      Text(viewModel.date)
     }
   }
 }
 
+//MARK: - Preview
 #Preview {
-  NewsCell()
+  NewsCell(
+    viewModel: NewsViewModel(
+      title: "고양이가 그렇게 귀엽다며... 사료를 1년치 주문... 충격",
+      press: "카카오또-끄",
+      createdDate: .init(timeIntervalSinceNow: -60*60)
+    )
+  )
 }
