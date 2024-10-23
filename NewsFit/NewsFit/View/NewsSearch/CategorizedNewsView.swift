4798e96a2a11ca79b7fd4b69f4abac38074300e3
iceHood <hanyo3477@gmail.com>
2024-10-23 13:15:40 +0900

    refactor: 카테고리 뉴스 뷰 별도 분리

--- NewsFit/NewsFit/View/NewsSearch/CategorizedNewsView.swift
+++ NewsFit/NewsFit/View/NewsSearch/CategorizedNewsView.swift
@@ -1,59 +1,6 @@
 import SwiftUI
 
-struct NewsFitSearchView: View {
-  @State private var text = ""
-  var body: some View {
-    searchBar()
-    recentlySearched()
-    Divider()
-    ScrollView {
-      VStack(alignment: .leading, spacing: 20) {
-        ForEach(0..<10) { index in
-          CategorizedNewsView()
-            .frame(height: 200)
-            .padding(.horizontal)
-        }
-      }
-    }
-  }
-  
-  @ViewBuilder
-  private func searchBar() -> some View {
-    HStack {
-      TextField("키워드 / 언론사 / 카테고리로 검색", text: $text)
-      Image(.nfSearchButton)
-    }
-    .padding(.horizontal)
-    .padding(.vertical, 10)
-    .modifier(NFBorderModifier())
-    .padding(.horizontal)
-  }
-  @ViewBuilder
-  private func recentlySearched() -> some View {
-    VStack {
-      HStack {
-        Text("최근검색어")
-          .font(.headline)
-        Spacer()
-        Button(action: {}) {
-          Text("모두 지우기")
-            .font(.subheadline)
-            .foregroundColor(.secondary)
-        }
-      }
-      ScrollView(.horizontal) {
-        HStack {
-          ForEach(0..<10) { index in
-            NewsCategoryCell(viewModel: .init(value: "\(index)"))
-          }
-        }
-      }
-    }
-    .padding()
-  }
-}
-
-fileprivate struct CategorizedNewsView: View {
+struct CategorizedNewsView: View {
   var body: some View {
     VStack(alignment: .leading) {
       Text("[어떤] 카테고리 최신 뉴스")
@@ -110,5 +57,5 @@ fileprivate struct CategorizedNewsView: View {
 }
 
 #Preview {
-  NewsFitSearchView()
+  CategorizedNewsView()
 }
