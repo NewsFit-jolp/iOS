0fe3901ff8a811da39e825f1b7b3b9ff5761fa70
iceHood <hanyo3477@gmail.com>
2024-11-20 09:39:04 +0900

    feat: User선호 기사들 가져옴

--- NewsFit/NewsFit/Repository/UserService.swift
+++ NewsFit/NewsFit/Repository/UserService.swift
@@ -57,6 +57,33 @@ final class UserService {
     }
     
     return .success(decoded.result)
+  }
+  func fetchUserPress() async -> Result<[String], Error> {
+    let baseURL = Bundle.baseURL
+    let path = "/member/press"
+    let token = Bundle.token
     
+    let request = HTTPRequestBuilder(baseURL: baseURL, path: path, method: .get)
+      .update(headers: ["Authorization": "Bearer \(token)",
+                        "Content-Type": "application/json"])
+      .build()
+    
+    guard let data = try? await HTTPServiceProvider().fetchData(for: request).get() else {
+      return .failure(UserServiceError.invalidData)
+    }
+    
+    guard let decoded = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else {
+      return .failure(UserServiceError.invalidJSON)
+    }
+    
+    guard let result = decoded["result"] as? [String: [String]] else {
+      return .failure(UserServiceError.invalidJSON)
+    }
+    
+    guard let preferredPress = result["preferredPress"] else {
+      return .failure(UserServiceError.invalidJSON)
+    }
+    
+    return .success(preferredPress)
   }
 }
